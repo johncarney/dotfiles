0dc07b596fe16f870bd3cafdfff9315f9edfb8e4
John Carney <john@carney.id.au>
2014-07-11 11:59:04 +1000

    Updated RVM and removed RubyMine guff.

--- bashrc.sh
+++ bashrc.sh
@@ -8,14 +8,17 @@ if [ -f `brew --prefix`/etc/bash_completion ]; then
     . `brew --prefix`/etc/bash_completion
 fi
 
-export RUBYMINE=/Applications/RubyMine.app
-export RUBYLIB="$RUBYMINE/rb/testing/patch/common:$RUBYMINE/rb/testing/patch/bdd"
+# export RUBYMINE=/Applications/RubyMine.app
+# export RUBYLIB="$RUBYMINE/rb/testing/patch/common:$RUBYMINE/rb/testing/patch/bdd"
 
 export NODE_PATH=/usr/local/lib/node_modules
 
 export RUBY_GC_MALLOC_LIMIT=90000000
 export RUBY_FREE_MIN=200000
 export RUBY_GC_HEAP_FREE_SLOTS=200000
+
+alias ls='ls -Al'
+alias lsh='ls -Alh'
 
 # Git aliases
 alias gco='git checkout'
@@ -26,20 +29,24 @@ alias grc='git rebase --continue'
 __git_complete gco _git_checkout
 
 # Most recent Rails migration
-alias lm='ls db/migrate/*.rb | tail -n 1'
+alias lm='ls -1 db/migrate/*.rb | tail -n 1'
 
 # Zeus aliases
-alias zrspec='zeus rspec -fd --order default --no-profile'
+alias zrspec='zeus rspec -fd --order defined --no-profile'
 alias zconsole='zeus console'
 alias zstart='zeus start'
 alias zrake='zeus rake'
 alias zgenerate='zeus generate'
 
-alias xrspec='xargs zeus rspec -fd --order default --no-profile'
+# xarg aliases
+alias xrspec='xargs zeus rspec -fd --order defined --no-profile'
+alias xsublime='xargs sublime'
 
-source ~/.profile
-source ~/.git-prompt.sh
+# source ~/.profile
+# source ~/.git-prompt.sh
 
 export PATH="/usr/local/bin:${PATH//\/usr\/local\/bin:/}"
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
