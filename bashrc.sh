fc35e6f5078f7a8827684a44d785404b92c32e39
John Carney <john@carney.id.au>
2019-10-10 20:05:56 -0600

    Add ./git/safe/../../bin to PATH. Not entirely sure why...

--- bashrc.sh
+++ bashrc.sh
@@ -13,14 +13,13 @@ if [ -n "$(type -t brew)" ]; then
     export BREW_PREFIX="$(brew --prefix)"
 fi
 
-export PATH="./bin:~/bin:$PATH"
-
 if [ -n "$(type -t rbenv)" ]; then
     export PATH="$(rbenv root):$PATH"
     eval "$(rbenv init -)"
 elif [ -t "$HOME/.rvm/scripts/rvm" ]; then
     source "$HOME/.rvm/scripts/rvm"
 fi
+export PATH=".git/safe/../../bin:~/bin:$PATH"
 
 if [ -d "$HOME/.yarn/bin" ]; then
     export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
